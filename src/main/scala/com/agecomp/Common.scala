6b3e8983f6e217b2bb0c38e9622095d21bfec820
Abdiel Brilhante <abdielbrilhante.uece@gmail.com>
2017-05-04 15:27:26 -0300

    Add instance() template function
    Generic function to generate classes from a String representing the
    class' name, but as an instance of the base class, to be used to create Processor and Component derived objects.

--- src/main/scala/com/agecomp/Common.scala
+++ src/main/scala/com/agecomp/Common.scala
@@ -3,4 +3,9 @@ package com
 package object agecomp {
   import scala.collection.mutable.HashMap
   type ComponentMap = HashMap[Int, Component]
+
+  // TODO: add tuple arg for construtor parameters
+  def instance[T](className: String): T = {
+    Class.forName(className).newInstance().asInstanceOf[T]
+  }
 }
