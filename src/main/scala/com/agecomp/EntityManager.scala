5378f78e4f8bd74b4dcae89be57f8242028d9918
Abdiel Brilhante <abdielbrilhante.uece@gmail.com>
2017-05-05 09:41:31 -0300

    Add entity generation from blueprint

--- src/main/scala/com/agecomp/EntityManager.scala
+++ src/main/scala/com/agecomp/EntityManager.scala
@@ -3,4 +3,22 @@ package com.agecomp
 import scala.collection.mutable.HashMap
 
 class EntityManager(val components: HashMap[String, ComponentMap]) {
+  private var _id: Int = 0
+
+  // TODO: Better uid generation
+  def uid: Int = { _id += 1; _id }
+
+  // TODO: Check if component name is valid
+  def construct(blueprint: List[(String, Product)]) = {
+    blueprint.foreach(component => {
+      val componentName = component._1
+
+      if (!components.contains(componentName)) {
+        components.put(componentName, new ComponentMap())
+      }
+
+      val container = components(componentName)
+      container.put(uid, instance(componentName))
+    })
+  }
 }
