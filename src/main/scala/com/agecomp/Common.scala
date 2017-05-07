a8e8a9b205ac89d38e8a8d95e9603ca9a4941064
Abdiel Brilhante <abdielbrilhante.uece@gmail.com>
2017-05-07 14:14:50 -0300

    Added ID generator object

--- src/main/scala/com/agecomp/Common.scala
+++ src/main/scala/com/agecomp/Common.scala
@@ -7,4 +7,9 @@ package object agecomp {
   def instance[T](className: String, init_args: AnyRef*): T = {
     Class.forName(className).getConstructors.head.newInstance(init_args: _*).asInstanceOf[T]
   }
+
+  object IDGenerator {
+    private var _id: Int = -1
+    def uid: Int = { _id += 1; _id }
+  }
 }
