3d9f15cbe79e7cb57ff6501c2f19aecf6d3f619b
Abdiel Brilhante <abdielbrilhante.uece@gmail.com>
2017-06-15 19:45:02 -0300

    Add Energy component

---
+++ src/main/scala/com/agecomp/energy/Components.scala
@@ -0,0 +1,9 @@
+package com.agecomp.energy
+
+import com.agecomp.Component
+import com.agecomp.InputComponent
+
+class AbsorbInput() extends InputComponent
+
+class Energy(var value: Int) extends Component {
+}
