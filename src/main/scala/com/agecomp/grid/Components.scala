dbc3bd2fcd0e5cf0cb54772e4727adeb74fde581
Abdiel Brilhante <abdielbrilhante.uece@gmail.com>
2017-07-18 15:46:01 -0300

    Remove example agent

--- src/main/scala/com/agecomp/grid/Components.scala
+++ src/main/scala/com/agecomp/grid/Components.scala
@@ -51,27 +51,3 @@ class JFXComponent(val node: Node) extends Component {
     })
   }
 }
-
-class Dumbo extends Actor {
-  val id = self.path.name.toInt
-
-  val input = new MovementInput("Right")
-
-  def randDirection: String = {
-    val r = scala.util.Random
-    val i = r.nextInt(4)
-    return i match {
-      case 0 => "Up"
-      case 1 => "Down"
-      case 2 => "Left"
-      case 3 => "Right"
-      case _ => "Down"
-    }
-  }
-
-  def receive = {
-    case _ =>
-      input.direction = randDirection
-      sender ! input
-  }
-}
